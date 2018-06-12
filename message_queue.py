995362c18ea6849794982b812bf72412025384ef
chengluyu <chengluyu@live.cn>
2018-06-12 13:52:49 +0800

    add sensor to message queue

--- message_queue.py
+++ message_queue.py
@@ -1,6 +1,8 @@
+import collections
 import threading
 import time
-import collections
+
+import sensor
 
 
 class Routine:
@@ -22,6 +24,28 @@ _routines = dict()
 _events = dict()
 _end_loop = False
 _is_running = False
+
+_last_sensor_status = dict()
+_sensors = [['infra', sensor.infrared_sensors],
+            ['track', sensor.track_detectors]]
+
+
+def on(event_name, task_name_or_func):
+    if event_name in _events:
+        _events[event_name].append(task_name_or_func)
+    else:
+        _events[event_name] = [task_name_or_func]
+
+
+def trigger(name, *args, **kwargs):
+    if name in _events:
+        for handler in _events:
+            if callable(handler):
+                handler(*args, **kwargs)
+            elif type(handler) == str:
+                execute(handler)
+            else:
+                print('Warning: event "{}" is not a callable object or task name')
 
 
 def task(name, func):
@@ -76,7 +100,11 @@ def loop_body():
     _is_running = True
     while not _end_loop:
         # check status of sensors
-
+        for sensor_name, sensor_getter in _sensors:
+            new_status = sensor_getter()
+            if _last_sensor_status.get(sensor_name) != new_status:
+                trigger(sensor_name, new_status)
+                _last_sensor_status[sensor_name] = new_status
         # execute all routines
         for item in _queue:
             item.remaining_cycle -= 1
@@ -85,7 +113,8 @@ def loop_body():
                 if item.once:
                     item.to_be_remove = True
                 item.remaining_cycle = item.interval
-        _queue = collections.deque(filter(lambda x: not x.to_be_remove, _queue))
+        _queue = collections.deque(
+            filter(lambda x: not x.to_be_remove, _queue))
         time.sleep(_MESSAGE_QUEUE_TICK)
     _end_loop = False
     _is_running = False
--- test_find_track.py
+++ test_find_track.py
@@ -9,15 +9,22 @@ def stop_queue():
     mq.stop()
 
 
+# queue actions
 mq.task('stop_queue', stop_queue)
+
+# actions of car
+mq.task('go', car.move_forward)
+mq.task('back', car.move_backward)
 mq.task('stop', car.stop)
 mq.task('turn-left', car.turn_left)
 mq.task('turn-right', car.turn_right)
 
-mq.execute('turn-left')
-mq.timeout('turn-right', 100)
-mq.timeout('stop', 200)
+# keep going until track detects
+mq.execute('go')
+mq.on('track', 'stop')
+
+# stop car after 3 seconds
 mq.timeout('stop_queue', 300)
 
+# start the process
 mq.start()
-mq.join()
