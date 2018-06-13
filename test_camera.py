158cc7c58cecfd904724737f059195a847238407
z-jack <emailjiong@126.com>
2018-06-13 17:30:28 +0800

    Fix image read error in camera.

--- test_camera.py
+++ test_camera.py
@@ -9,7 +9,7 @@ with picamera.PiCamera() as camera:
     stream = io.BytesIO()
     for foo in camera.capture_continuous(stream, format='jpeg', use_video_port=True):
         data = np.frombuffer(stream.getvalue(), dtype=np.uint8)
-        image = cv2.imdecode(data, cv2.CV_LOAD_IMAGE_UNCHANGED)
+        image = cv2.imdecode(data, cv2.IMREAD_UNCHANGED)
         gray = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
 
         circles1 = cv2.HoughCircles(gray, cv2.HOUGH_GRADIENT, 1, 600, param1=100, param2=30, minRadius=10,
