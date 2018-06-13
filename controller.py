01c8684dd6340ef60991b33af4e3b81f2b7280c1
chengluyu <chengluyu@live.cn>
2018-06-13 13:09:38 +0800

    change pins of motors

--- controller.py
+++ controller.py
@@ -3,12 +3,34 @@ import time
 
 __all__ = ['Vehicle']
 
-ENA = 11
-IN1 = 13
-IN2 = 15
-IN3 = 19
-IN4 = 21
-ENB = 23
+#        3V3  (1) (2)  5V
+#      GPIO2  (3) (4)  5V
+#      GPIO3  (5) (6)  GND
+#      GPIO4  (7) (8)  GPIO14
+#        GND  (9) (10) GPIO15
+#     GPIO17 (11) (12) GPIO18
+#     GPIO27 (13) (14) GND
+#     GPIO22 (15) (16) GPIO23
+#        3V3 (17) (18) GPIO24
+#     GPIO10 (19) (20) GND
+#      GPIO9 (21) (22) GPIO25
+#     GPIO11 (23) (24) GPIO8
+#        GND (25) (26) GPIO7
+#      GPIO0 (27) (28) GPIO1
+#      GPIO5 (29) (30) GND
+#      GPIO6 (31) (32) GPIO12
+# IN2 GPIO13 (33) (34) GND
+# IN1 GPIO19 (35) (36) GPIO16 IN3
+# ENA GPIO26 (37) (38) GPIO20 IN4
+#        GND (39) (40) GPIO21 ENB
+
+
+ENA = 37
+IN1 = 35
+IN2 = 33
+IN3 = 36
+IN4 = 38
+ENB = 40
 ALL_PINS = [ENA, IN1, IN2, IN3, IN4, ENB]
 
 
