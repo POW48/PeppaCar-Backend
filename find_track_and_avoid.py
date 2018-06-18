fd521d3cef7ca9247549b52369f7d0ac95cf3e4c
talegqz <790717887@qq.com>
2018-06-18 11:10:45 +0800

    maybe finish find track and avoid ob

--- find_track_and_avoid.py
+++ find_track_and_avoid.py
@@ -1,24 +1,57 @@
 import car
 import scheduler
 
+track_flag =1
 
+#1 nomorl find track
+#2 return track
+#3 nothing
 
-def simple_steer_track():
-    def track_detector_callback(status):
-        scheduler.cancel('avoid_ob')
+def track_detector_callback(status):
+
+    if track_flag==1:
         left, middle, right = status
         if left == 1 and middle == 0:
-            rotate_left()
+            car.rotate_left()
         if right == 1 and middle == 0:
-            rotate_right()
+            car.rotate_right()
         if middle == 1:
-            go()
-    on_track_detector_change(track_detector_callback)
-    go()
+            car.go()
 
+    elif track_flag==3:
+        pass
+    else:
+        if middle==1:
+            scheduler.cancel('avoid_ob')
+            change_flag_normal()
+            car.rotate_left()
+
+
+
+def change_flag_normal():
+    global track_flag
+    track_flag = 1
+def change_flag_nothing():
+    global track_flag
+    track_flag = 3
+def change_flag_return():
+    global track_flag
+    track_flag = 2
+def simple_avoid_ob(status):
+    left, middle, right = status
+
+
+    if middle==1:
+        change_flag_nothing
+        car.rotate_left()
+        scheduler.schedule('avoid_ob',(70,change_flag_return),(1,car.go),(70,car.rotate_right),(70,car.go))
+
+    pass
 
 
 if __name__ == '__main__':
-    simple_steer_track()
+    car.on_track_detector_change(track_detector_callback)
+    car.on_infrared_sensor_change(simple_avoid_ob)
+    go()
 
 
