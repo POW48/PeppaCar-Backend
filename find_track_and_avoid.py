b7475fb1d16b4315a272a45082574079056324b9
gqz <790717887@qq.com>
2018-06-19 22:38:18 +0800

    FFF

--- find_track_and_avoid.py
+++ find_track_and_avoid.py
@@ -84,7 +84,7 @@ def simple_avoid_ob(status):
 
 def set_wheel_to_rotate():
     car.set_left_wheels_speed(10)
-    car.set_right_wheels_speed(1)
+    car.set_right_wheels_speed(0.0001)
 
 
 def recover_wheel_to_normal():
