f537912119660033f135eb9273ff345121e34cff
chengluyu <chengluyu@live.cn>
2018-06-20 04:07:57 +0800

    try different speed on left and right wheels

--- ball_utils.py
+++ ball_utils.py
@@ -96,7 +96,8 @@ def go_door():
 
 
 if __name__ == '__main__':
-    car.set_global_speed(1)
+    car.set_left_wheels_speed(1)
+    car.set_right_wheels_speed(2)
     camera = picamera.PiCamera()
     camera.resolution = (320, 240)
     camera.framerate = 60
